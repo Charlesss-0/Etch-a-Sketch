a7b2b9f5be522d062efd14706c62423df17edcd5
carlos505 <aragonaugusto24@gmail.com>
2023-06-26 12:26:37 -0600

    Fix(gridContainer)

--- script.js
+++ script.js
@@ -9,17 +9,8 @@ rangeInputValue.addEventListener('input', () => {
     rangeValueNum.textContent = value
 })
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        let squares = document.createElement('div')
-        squares.classList.add('square')
-        gridContainer.style.gridTemplateColumns = `repeat(16, 1fr)`
-        gridContainer.style.gridTemplateRows = `repeat(16, 1fr)`
-        gridContainer.appendChild(squares)
-    }
-}
-
 function getInputValue(value) {
+    gridContainer.innerHTML = ''
     for (let i = 0; i < value; i++) {
         for (let j = 0; j < value; j++) {
             let squares = document.createElement('div')
@@ -29,6 +20,10 @@ function getInputValue(value) {
             gridContainer.appendChild(squares)
         }
     }
+    
+    addClickListeners()
+    addHoverListeners()
+    changeAllSquareColors()
 }
 
 applyBtnEl.addEventListener('click', () => {
@@ -47,48 +42,56 @@ const gridEl = document.querySelector('.grid')
 const fillEl = document.querySelector('.fill-el')
 const opacityEl = document.querySelector('.opacity')
 
-const sketchSectionEl = document.querySelector('#sketch-section')
-
-function moveSketch() {
-    sketchSectionEl.classList.toggle('sketch-margin')
-}
-
 paletteIconEl.addEventListener('click', () => {
     colorEl.classList.toggle('color')
-    // moveSketch()
 })
 
 gridIconEl.addEventListener('click', () => {
     gridEl.classList.toggle('grid')
-    // moveSketch()
 })
 
 fillIconEl.addEventListener('click', () => {
     fillEl.classList.toggle('fill-el')
-    // moveSketch()
 })
 
 opacityIconEl.addEventListener('click', () => {
     opacityEl.classList.toggle('opacity')
-    // moveSketch()
 })
 
 // Icon options
 const pickIconEl = document.querySelector('#pick-icon')
 const hoverIconEl = document.querySelector('#hover-icon')
 const fillGridContainer = document.querySelector('#fillGridContainer-icon')
-const squareEl = document.querySelectorAll('.square')
 const colorInputEl = document.querySelector('#color-input')
 
-const warmEl = document.querySelector('#warm-option')
-const coolEl = document.querySelector('#cool-option')
-const pastelEl = document.querySelector('#pastel-option')
+const warmEl = document.querySelector('.warm-option')
+// const coolEl = document.querySelector('.cool-option')
+// const pastelEl = document.querySelector('.pastel-option')
+
+const warmColorsArr = [
+    '#797d62',
+    '#9b9b7a',
+    '#baa587',
+    '#d9ae94',
+    '#f1dca7',
+    '#ffcb69',
+    '#e8ac65',
+    '#d08c60',
+    '#b58463',
+    '#997b66'
+]
+
+function getRandomColor(colorsArr) {
+    const randomIndex = Math.floor( Math.random() * colorsArr.length )
+    return colorsArr[randomIndex]
+}
 
 function changeSquareColor(element) {
-    element.style.backgroundColor = colorInputEl.value
-} // Gets the square color from the input value
+   element.style.backgroundColor = getRandomColor(warmColorsArr)
+}
 
 function addClickListeners() {
+    const squareEl = document.querySelectorAll('.square')
     squareEl.forEach((element) => {
         element.addEventListener('click', () => {
             changeSquareColor(element)
@@ -97,6 +100,7 @@ function addClickListeners() {
 } // Changes the square color when clicked
 
 function addHoverListeners() {
+    const squareEl = document.querySelectorAll('.square')
     squareEl.forEach((element) => {
         element.addEventListener('mouseover', () => {
             changeSquareColor(element)
@@ -105,6 +109,7 @@ function addHoverListeners() {
 } // Changes the square color when hovered
 
 function changeAllSquareColors() {
+    const squareEl = document.querySelectorAll('.square')
     squareEl.forEach((element) => {
         changeSquareColor(element)
     })
@@ -124,28 +129,12 @@ fillGridContainer.addEventListener('click', () => {
     changeAllSquareColors()
 })
 
-// function getColorSchemeValue(element) {
-//     element.style.backgroundColor = 
-// }
-
 warmEl.addEventListener('click', () => {
-    const colorsArr = [
-        '#797d62',
-        '#9b9b7a',
-        '#baa587',
-        '#d9ae94',
-        '#f1dca7',
-        '#ffcb69',
-        '#e8ac65',
-        '#d08c60',
-        '#b58463',
-        '#997b66'
-    ]
+    changeSquareColor(warmEl)
+})
 
-    const randomNum = Math.floor( Math.random() * colorsArr.length)
-    const selectedColor = colorsArr[randomNum]
-
-    addClickListeners(selectedColor)
+colorInputEl.addEventListener('change', () => {
+    changeSquareColor(colorInputEl)
 })
 
 // Opacity options
@@ -153,6 +142,7 @@ const opacityOneEl = document.querySelector('#opacity-50')
 const opacityTwoEl = document.querySelector('#opacity-100')
 
 function getOpacityValue(opacityVal) {
+    const squareEl = document.querySelectorAll('.square')
     squareEl.forEach((element) => {
         element.style.opacity = opacityVal
     })
@@ -165,69 +155,3 @@ opacityOneEl.addEventListener('click', () => {
 opacityTwoEl.addEventListener('click', () => {
     getOpacityValue('100%')
 })
-
-// grid16.addEventListener('click', () => {
-//     const gridValue = 16
-//     getInputValue(gridValue)
-// })
-
-// grid32.addEventListener('click', () => {
-//     const gridValue = 32
-//     getInputValue(gridValue)
-// })
-
-// grid64.addEventListener('click', () => {
-//     const gridValue = 64
-//     getInputValue(gridValue)
-// })
-
-// createGrid = gridNumber => {
-//     if (gridNumber === undefined) {
-//         gridNumber = 16
-//     } else gridNumber = gridNumber
-
-//     let gridSize = gridNumber * gridNumber
-//     for (i = 0; i < gridSize; i++) {
-//         let squares = document.createElement('div')
-//         squares.classList.add('squares')
-//         div.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`
-//         div.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`
-//         div.appendChild(squares)
-//     }
-// }
-
-// function createGrid(gridNumber) {
-//     if (gridNumber === undefined) {
-//         gridNumber = 16
-//     } else {
-//         gridNumber = gridNumber
-//     }
-
-//     let gridSize = gridNumber * gridNumber
-//     for (let i = 0; i < gridSize; i++) {
-//         for (let j = 0; j < gridSize; j++) {
-//             let squares = document.createElement('div')
-//             squares.classList.add('squares')
-//             div.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`
-//             div.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`
-//             gridContainer.appendChild(squares)
-//         }
-//     }
-// }
-
-// applyBtnEl.addEventListener('click', () => {
-//     const valueEl = rangeInputValue.value
-//     if (typeof valueEl === 'undefined') {
-//         return 
-//     } else {
-//         createGrid(valueEl)
-//     }
-// })
-
-// for (let i = 0; i < 16; i++) {
-//     for (let j = 0; j < 16; j++) {
-//         gridContainer.innerHTML += `
-//             <div class="square"></div>
-//         `
-//     }
-// } // creates a 16x16 grid using div
